2f0cd0bcf5e1a2fd03e680f98a5c3bf429455e07
aidityas <aidityasadhakim250@gmail.com>
2022-09-18 09:40:30 +0700

    Add comment to config.py

--- config.py
+++ config.py
@@ -1,8 +1,13 @@
 API_KEY = "API KEY HERE"
 API_SECURITY = "API SECURITY HERE"
 
+# Frequency of the refresh order check
 CHECK_ORDERS_FREQUENCY = 1
+
+# Status if the order is closed
+# Note that every exchange have different status
 CLOSED_ORDER_STATUS = "FILLED"
 
+# Set your own stop loss to minimize loss
 STOP_LOSS = 0.05
 
